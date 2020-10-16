28dd3f8974bfc4fc1d0e3ab201fc07dc848c2135
carinapeng <fpeng@college.harvard.edu>
2020-10-15 20:18:03 -0500

    Create basic plots

--- plot.R
+++ plot.R
@@ -1,34 +1,42 @@
 
 # Load libraries
-library("readxl")
-library("magrittr")
-library("janitor")
-library("ggplot2")
-library("dplyr")
+library(readxl)
+library(magrittr)
+library(janitor)
+library(ggplot2)
+library(dplyr)
 
 # Read data
-x <- read_excel("/Users/carinapeng/PAHO : WHO/who-noncommunicable-diseases/excel/Input tables for Carina_6Oct2020.xlsx", sheet = 3)
+gbd <- read_excel("/Users/carinapeng/PAHO : WHO/who-noncommunicable-diseases/data/Input tables for Carina_6Oct2020.xlsx", sheet = 3)
+pop <- read_excel("/Users/carinapeng/PAHO : WHO/who-noncommunicable-diseases/data/Input tables for Carina_6Oct2020.xlsx", sheet = 4)
 
 # Clean data
-x <- x %>% 
-  clean_names()
+gbd_country <- gbd %>% 
+  clean_names() %>%
+  filter(country == "Afghanistan", sex == "Male")
 
-# create dummy data
-don <- data.frame(
-  x = rep(seq(2000,2005), 3),
-  value = c(  75, 73, 68, 57, 36, 0, 15, 16, 17, 18, 19, 20, 10, 11, 15, 25, 45, 80),
-  group = rep(c("A", "B", "C"), each=6)
-)
+pop_country <- pop %>% 
+  clean_names() %>%
+  filter(country == "Afghanistan" & sex == "Male") %>%
+  select(upper_age, value)
 
-x %>%
-  filter(sex == "Male")
+# Join GBD and Population data
+
+joined <- inner_join(pop_country, gbd_country) %>%
+  mutate(people = value * percentage_of_population)
+
 
 # Plot a stacked density plot
-plot <- x %>% 
-  filter(country == "Afghanistan", sex == "Male") +
-  ggplot(aes(x=gbd_location_name, y=percentage_of_population, fill=condition)) +
-  geom_area( ) +
-  scale_fill_viridis(discrete = TRUE) +
-  theme(legend.position="none") +
-  theme_ipsum() +
-  theme(legend.position="none")
+
+plot_prev <- joined %>% 
+  ggplot(aes(x=upper_age, y=percentage_of_population, fill=condition)) +
+  geom_area( )
+
+plot_pop <- joined %>%
+  ggplot(aes(x=upper_age, y=people, fill=condition)) +
+  geom_area( )
+
+
+
+
+
