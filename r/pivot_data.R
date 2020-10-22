688bfb90ed296cf1e0ae00e46a6cb945509c3d1b
carinapeng <fpeng@college.harvard.edu>
2020-10-22 13:35:26 -0500

    Pivot data capitalized country

--- r/pivot_data.R
+++ r/pivot_data.R
@@ -11,10 +11,26 @@ library(tidyverse)
 
 pop1 <- read_excel("/Users/carinapeng/PAHO : WHO/who-noncommunicable-diseases/data/Input tables for Carina_6Oct2020.xlsx", sheet = 1)
 
-pop1_clean <- pop1 %>% 
+
+pop1_pivot <- pop1 %>%
+  pivot_longer(cols = !c(1, 2, 3, 4, 5),
+  #pivot_longer(cols = !c(indicator, sex, lower_age, upper_age, gbd_location_name),
+               names_to = "country",
+               values_to = "value") %>%
   clean_names()
 
-pop1_pivot <- pop1_clean %>%
-  pivot_longer(cols = !c(indicator, sex, lower_age, upper_age, gbd_location_name),
+gbd1 <- read_excel("/Users/carinapeng/PAHO : WHO/who-noncommunicable-diseases/data/Input tables for Carina_6Oct2020.xlsx", sheet = 2)
+
+gbd1_pivot <- gbd1 %>%
+  pivot_longer(cols = !c(1:5),
                names_to = "country",
-               values_to = "value")
+               values_to = "value") %>%
+  clean_names()
+
+afg <- gbd1_pivot %>%
+  filter(country == "Afghanistan")
+
+
+
+
+
